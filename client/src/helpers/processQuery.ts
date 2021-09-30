9cb2e514b794974ba8c1ecf2ad8d0d9797a96613
Jure Bogunovic <bogunovij@gmail.com>
2021-10-01 00:29:11 +0200

    set gas price to 1.5 gwei

--- client/src/helpers/processQuery.ts
+++ client/src/helpers/processQuery.ts
@@ -1,16 +1,19 @@
 import { Web3ApiClient } from "@web3api/client-js";
-import { BytesLike } from "ethers";
-import { defaultAbiCoder, keccak256, solidityKeccak256 } from "ethers/lib/utils";
+import { BytesLike, ethers } from "ethers";
+import {
+  defaultAbiCoder,
+  keccak256
+} from "ethers/lib/utils";
 import { IPEON } from "../typechain";
 import { getPolywrapperMethodMap } from "./getPolywrapperMethodMap";
 import { query } from "./query";
 
 export const processQuery = async (
   polywrapClient: Web3ApiClient,
-  peonContract: IPEON, 
-  queryId: BytesLike, 
-  packageUri: string, 
-  func: BytesLike, 
+  peonContract: IPEON,
+  queryId: BytesLike,
+  packageUri: string,
+  func: BytesLike,
   args: BytesLike
 ) => {
   const polywrapperUri = packageUri;
@@ -18,30 +21,41 @@ export const processQuery = async (
   const methods = await getPolywrapperMethodMap(polywrapClient, polywrapperUri);
 
   const methodInfo = methods[func.toString()];
-  if(!methodInfo) {
+  if (!methodInfo) {
     throw "Function with the specified signature does not exist";
   }
 
-  let decodedArgs: any[] = defaultAbiCoder.decode(methodInfo.argTypes, args) as unknown as any;
+  let decodedArgs: any[] = defaultAbiCoder.decode(
+    methodInfo.argTypes,
+    args
+  ) as unknown as any;
 
-  const response = await query(polywrapClient, polywrapperUri, methodInfo.queryOrMutation, methodInfo.methodName,
-    methodInfo.argNames, decodedArgs)
+  const response = await query(
+    polywrapClient,
+    polywrapperUri,
+    methodInfo.queryOrMutation,
+    methodInfo.methodName,
+    methodInfo.argNames,
+    decodedArgs
+  );
 
   let responseData: BytesLike = defaultAbiCoder.encode(
-    ["bytes32"].concat(methodInfo.returnTypes), 
+    ["bytes32"].concat(methodInfo.returnTypes),
     [queryId, response]
   );
 
-  const responseHash = keccak256(
-    responseData
-  );
+  const responseHash = keccak256(responseData);
+
+  console.log("Sending response...");
 
   const respondTx = await peonContract.respond(
     queryId,
     responseHash,
-    responseData
+    responseData,
+    {
+      gasPrice: ethers.utils.parseUnits("1.5", "gwei"),
+    }
   );
 
   await respondTx.wait();
 };
-
